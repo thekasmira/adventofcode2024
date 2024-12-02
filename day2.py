edeb9d5d8485475cf75f1b6cb68d8471c10aa30e
thekasmira <kasmira.d.stockton@gmail.com>
2024-12-02 12:37:07 -0500

    added second part of challenge

--- day2.py
+++ day2.py
@@ -13,8 +13,22 @@ def is_safe_report(report):
     
     return False
 
+def can_be_safe_with_one_removal(report):
+    if is_safe_report(report):
+        return True
+
+    for i in range(len(report)):
+        modified_report = report[:i] + report[i+1:]
+        if is_safe_report(modified_report):
+            return True
+
+    return False
+
 def count_safe_reports(reports):
     return sum(1 for report in reports if is_safe_report(report))
+
+def count_safe_reports_with_removal(reports):
+    return sum(1 for report in reports if can_be_safe_with_one_removal(report))
 
 def main():
     path = "/Users/kasmira/Documents/Code/adventofcode2024/text2.txt"
@@ -25,7 +39,9 @@ def main():
         return
         
     safe_count = count_safe_reports(report_list)
+    safe_with_exceptions_count = count_safe_reports_with_removal(report_list)
     print("The number of safe reports is: ", safe_count)
+    print("The number of reports that are safe with one or less changes: ", safe_with_exceptions_count)
 
 if __name__ == "__main__":
     main()
